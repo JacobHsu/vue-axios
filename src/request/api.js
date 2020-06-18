909dd0744b62fbebda55644b10a32a39a23d9e67
jacobhsu <jacob.hsu.tw@gmail.com>
2020-06-18 17:54:08 +0800

    fix gh-pages publicPath

--- src/request/api.js
+++ src/request/api.js
@@ -2,10 +2,12 @@
  * api接口统一管理
  */
 import { api } from './http'
+
 // https://raw.githubusercontent.com/JacobHsu/vue-axios/master/db.json
 // export const apiComments = p => api.get('http://localhost:3000/comments', p);
 // export const apiGetPosts = p => api.get('http://localhost:3000/posts', p);
-export const apiComments = p => api.get('/comments.json', p);
-export const apiGetPosts = p => api.get('/posts.json', p);
+const publicPath = process.env.NODE_ENV === "production" ? "/vue-axios" : "";
+export const apiComments = p => api.get( publicPath + '/comments.json', p);
+export const apiGetPosts = p => api.get( publicPath + '/posts.json', p);
 export const delay2s = p => api.delay(2000, p)
 export const apiAddress = p => api.post('api/v1/users/my_address/address_edit_before', p);
